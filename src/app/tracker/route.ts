70f4ca0bd28c3fb72c6c394846b8dc5355f389ea
Mohammad Danish <thatsdanish02@gmail.com>
2026-07-28 06:27:06 +0530

    Add no-cache headers and iframe versioning to ensure immediate updates

--- src/app/tracker/route.ts
+++ src/app/tracker/route.ts
@@ -3,5 +3,12 @@ import { join } from "node:path";
 
 export async function GET() {
   const html = await readFile(join(process.cwd(), "index.html"), "utf8");
-  return new Response(html, { headers: { "content-type": "text/html; charset=utf-8" } });
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+      "cache-control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      "pragma": "no-cache",
+      "expires": "0",
+    },
+  });
 }
